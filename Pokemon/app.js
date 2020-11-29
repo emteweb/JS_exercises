1ece67c71a54d12bdf69f06e0497cc356282056c
Marcin T <fake_mail@gmail.com>
2020-11-29 20:04:38 +0100

    first commit

---
+++ Pokemon/app.js
@@ -0,0 +1,15 @@
+const baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+const container = document.querySelector('#container');
+
+for(let i = 1; i < 101; i++){
+    const pokemon = document.createElement('div');
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src = `${baseUrl}${i}.png`
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+
+    container.appendChild(pokemon);
+}
